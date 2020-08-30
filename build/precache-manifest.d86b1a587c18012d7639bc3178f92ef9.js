ca965695b16989edac40054f491f11500c1876f4
Joe Cox <joecox@live.co.uk>
2020-08-30 02:17:27 +0100

    adding build for pages

---
+++ build/precache-manifest.d86b1a587c18012d7639bc3178f92ef9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec33cc14184e50bf5732b7117f3babba",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "f673a2449a6fab01b01a",
+    "url": "/monsters/static/css/main.9e9a78ca.chunk.css"
+  },
+  {
+    "revision": "78adac64eaa07ff35dea",
+    "url": "/monsters/static/js/2.356d906f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters/static/js/2.356d906f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f673a2449a6fab01b01a",
+    "url": "/monsters/static/js/main.6e3c4bff.chunk.js"
+  },
+  {
+    "revision": "3dab8aa598718cb8e8b3",
+    "url": "/monsters/static/js/runtime-main.f48c3991.js"
+  }
+]);
